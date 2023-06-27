2d564fb581a34ef8ed29f1b2facb6178da8cab4d
Donghee <dongh94@naver.com>
2023-06-27 21:28:32 +0900

    Request mapping with annotations Params methods

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -22,5 +25,62 @@ public class RequestParamController {
         response.getWriter().write("ok");
     }
 
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
 
+    // 생략 가능 !!
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+
+    // 더 생략 가능 !! 근데 너무 과해..
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+
+    // 반드시 필요한 파라미터
+    // "" != null
+    // int 초깃값은 0 이라 null이 안되므로 500 에러 => Integer (Wrapper Class 초깃값은 null)
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+
+    // Default 설정도 가능
+    // 빈문자는 "" != null 이지만 Defalut의 경우 빈문자를 Default로 처리해준다.
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+
+    // Map 활용 가능
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap) {
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
 }
